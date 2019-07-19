8f42acc5f9d91905d59e61aa2f0258a659202f78
James Sundra <pojo226@gmail.com>
2019-07-19 02:45:54 -0700

    Add fixture def for ChauvetH6

--- src/Common/Fixtures/Types/Chauvet.ts
+++ src/Common/Fixtures/Types/Chauvet.ts
@@ -12,9 +12,14 @@ export class ChauvetQ12 implements FixtureDescriptor {
     public readonly components = { '4-Channel': [ 'RGB', 'A' ] };
 }
 
-
 export class ChauvetQUV12 implements FixtureDescriptor {
     public readonly name = 'Chauvet QUV12';
+    public readonly modes: FixtureModes[] = [ '6-Channel' ];
+    public readonly components = { '6-Channel': [ 'RGB', 'A', 'W', 'UV' ]};
+}
+
+export class ChauvetH6 implements FixtureDescriptor {
+    public readonly name = 'Chauvet H6';
     public readonly modes: FixtureModes[] = [ '6-Channel' ];
     public readonly components = { '6-Channel': [ 'RGB', 'A', 'W', 'UV' ]};
 }
